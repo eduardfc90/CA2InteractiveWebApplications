a74de86c4a916b627e7b17c7cf512ccc1bd21830
eduardfc90 <eduard.fercho@gmail.com>
2020-04-26 00:46:58 +0000

    saving datos in DB and printing

--- src/app.js
+++ src/app.js
@@ -1,6 +1,32 @@
-const express = require ('express');
+const express = require('express');
 const app = express();
 
-app.use( express.static(__dirname + '/public'));
+const Task = require('./model/Task');
 
-module.exports = app ;
+// settings
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', async (req, res) => {
+    const tasks = await Task.find();
+    res.render('index', {tasks });
+})
+
+// Create Task
+app.post('/create', async (req, res) => {
+    const newTask = new Task({
+        task: req.body.task,
+        description: req.body.description
+    });
+    await newTask.save();
+    res.redirect('/');
+})
+
+// STATIC Files
+app.use(express.static(__dirname + '/public'));
+
+module.exports = app;
